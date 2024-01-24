a8e0b3fac37c6a423662c362795ed7eaaf4eea93
Tanvir2003kaur <tanvirkaur264@gmail.com>
2024-01-24 08:56:59 -0500

    initial commit

---
+++ src/main/java/com/mycompany/git_demo_wednesday/Git_Demo_Wednesday.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.git_demo_wednesday;
+
+/**
+ *
+ * @author tanvir kaur
+ * @modifier tanvir kaur
+ */
+public class Git_Demo_Wednesday {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
